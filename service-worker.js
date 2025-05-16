f225cebc722501ade6c3d03eee6ff7fa9f68a5c3
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2025-05-16 17:02:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('zenbreak-cache').then(function (cache) {
-      return cache.addAll([
-        '/ZenBreak/',
+const CACHE_NAME = 'zenbreak-v1';
+const urlsToCache = [
+ '/ZenBreak/',
         '/ZenBreak/index.html',
   '/ZenBreak/choix-duree.html',
   '/ZenBreak/choix-son.html',
@@ -28,9 +26,43 @@ self.addEventListener('install', function (event) {
   '/ZenBreak/sounds/WINDDsgn_Vent (ID 0595)_LS.mp3',
   '/ZenBreak/sounds/gong.mp3',
   '/ZenBreak/sounds/notification.mp3'
-      ]).catch(function (error) {
-        console.error("Erreur lors du caching des ressources :", error);
-      });
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .catch(error => {
+        console.error("Erreur lors de la mise en cache :", error);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(error => {
+        console.error("Erreur lors du fetch :", error);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (!cacheWhitelist.includes(name)) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
   );
 });
